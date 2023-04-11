00c2ea612b97c182f77220b02b9fc1c0cded6ef1
Abhishek-1337 <abhishekrinku4@gmail.com>
2023-04-11 20:32:39 +0530

    env variables and config files added

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'});
+
 const app = require('./app');
+
 app.listen(3000, (req, res)=>{
     console.log('Listening on port 3000');
 })
