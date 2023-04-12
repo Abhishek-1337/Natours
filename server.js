01b705edc2806a0f58fb986f146eda5033325650
Abhishek-1337 <abhishekrinku4@gmail.com>
2023-04-12 17:32:28 +0530

    mongoose connection updated now using remote database(Atlas)

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
 
+const mongoose = require('mongoose');
+
 const app = require('./app');
+
+const DB = process.env.DB_URL.replace('<password>', process.env.DB_PASSWORD);
+console.log(DB);
+mongoose.connect(DB,{
+    useNewUrlParser: true,
+    
+})
+.then(()=>console.log('DB connection successful'));
 
 app.listen(3000, (req, res)=>{
     console.log('Listening on port 3000');
