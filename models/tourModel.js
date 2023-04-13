06375b11bf9e60a7c1e21190fd72d6df8abf283a
Abhishek-1337 <abhishekrinku4@gmail.com>
2023-04-13 17:11:04 +0530

    Improved tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,16 +4,49 @@ const tourSchema = mongoose.Schema({
     name:{
         type: String,
         required: [true, 'missing name value'],
-        unique: true
+        unique: true,
+        trim: true
     },
-    rating: {
+    duration:{
+        type: Number,
+        required: [true, 'A tour must have duration']
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have group size']
+    },
+    ratingsAverage:{
         type: Number,
         default: 4.5
     },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
+    },
+    priceDiscount: Number,
     price: {
         type: Number,
         required: [true, 'missing price value']
-    }
+    },
+    summary:{
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have summary']
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have image cover']
+    },
+    images:[String],
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    startDates: [Date]
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
