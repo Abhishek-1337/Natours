d3d9f6377162a10378bda47abb26616e0f85d855
Abhishek-1337 <abhishekrinku4@gmail.com>
2023-04-27 22:27:57 +0530

    check token for protected route

--- controllers/authController.js
+++ controllers/authController.js
@@ -50,3 +50,19 @@ exports.login = catchAsync(async (req, res, next)=>{
         token
     });
 });
+
+exports.protect = catchAsync(async(req, res, next) => {
+    //Check if token's there
+    let token;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        token = req.headers.authorization.split(' ')[1];
+    }
+
+    if(!token){
+        return next(new AppError('You are not logged in. Please login to get access', 401));
+    }
+    //Verify token if anybody has altered it or not
+    //Check if user still exist
+    //Check if user changes password after getting a token
+    next();
+});
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const tourController = require('../controllers/tourController');
+const authController = require('../controllers/authController');
 
 // router.param('id', tourController.checkID);
 router
@@ -17,7 +18,7 @@ router
 
 router
     .route('/')
-    .get(tourController.getAllTours)
+    .get(authController.protect, tourController.getAllTours)
     .post(tourController.createTour);
 
 router
