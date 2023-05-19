44c27ca00f0cbcd4719c6bd4e2fa97e06c01cd57
Abhishek-1337 <abhishekrinku4@gmail.com>
2023-05-19 15:41:30 +0530

    sending jwt with cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,9 +14,19 @@ const jwtSign = id => {
 
 const createSendToken = (user, statusCode, res) => {
     const token = jwtSign(user._id);
+    let cookieOptions = {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true
+    };
+
+    if(process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+    //attaching cookies to the response
+    res.cookie('jwt', token, cookieOptions);
     res.status(statusCode).json({
         status: "success",
-        token,
         data:{
             user: user
         }
