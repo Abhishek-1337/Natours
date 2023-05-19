221d7a5b3a3407b294ebf9025f6f461b26720e22
Abhishek-1337 <abhishekrinku4@gmail.com>
2023-05-19 15:51:41 +0530

    added different security layers defend from xss and nosql injections

--- app.js
+++ app.js
@@ -6,17 +6,33 @@ const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 if(process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
+//Add security headers
+app.use(helmet());
 
+//Sanitization nosql injections
+app.use(mongoSanitize());
 
+//Sanitize xss injections
+app.use(xss());
+
+//prevent parameter pollution
+app.use(hpp());
+
+//limit the requests from user
 const limiter = rateLimit({
     max: 100,
     windowMs: 60 * 60 * 1000,
     message: 'To many requests from this IP, please try again in one hour'
 })
 app.use('/api', limiter);
+
 //body parser, Reading data from body into req.body
 app.use(express.json());
 
