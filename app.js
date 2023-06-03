5b235dc4643d6151eefde5cfd941e55e7ffe7588
Abhishek-1337 <abhishekrinku4@gmail.com>
2023-06-03 17:55:41 +0530

    created base template in pug

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -13,6 +14,9 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 
 if(process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 //Add security headers
 app.use(helmet());
@@ -38,12 +42,20 @@ app.use(xss());
 app.use(hpp());
 
 //serving static files
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
 
+//Test middleware
 app.use((req, res, next)=>{
     req.requestTime = new Date().toISOString();
     next();
-})
+});
+
+//Routes
+app.get('/', (req, res) => {
+    res.status(200).render('base', {
+        tour: "The Forest Hiker"
+    });
+});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
