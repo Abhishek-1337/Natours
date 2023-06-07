4c654d7aa2a6ee33430c90287e0fb5f3364bf4b6
Abhishek-1337 <abhishekrinku4@gmail.com>
2023-06-07 12:06:30 +0530

    build overview page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,17 @@
-exports.getOverview = (req, res)=>{
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync( async(req, res)=>{
+    //Get all the tours from the database
+    const tours = await Tour.find();
+
+    //then create a template 
+    //pass tours to template
     res.status(200).render('overview', {
-        title: "All tours"
+        title: "All tours",
+        tours
     });
-};
+});
 
 exports.getTour = (req, res)=>{
     res.status(200).render('tour', {
