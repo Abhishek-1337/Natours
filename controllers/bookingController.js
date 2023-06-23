a162e7801e39c6c20fe06bf8fa70202a84be47e8
Abhishek-1337 <abhishekrinku4@gmail.com>
2023-06-23 17:20:26 +0530

    webhook checkout bug fixed

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -42,13 +42,13 @@ exports.getCheckoutSession = catchAsync( async(req, res, next) => {
     });
 });
 
-const createBookingCheckout = async (session) => {
+const createBookingCheckout = catchAsync(async (session) => {
     const tour = session.client_reference_id;
     const user = await User.findOne({email: session.customer_email});
-    const price = line_items[0].price_data.unit_amount / 100;
+    const price = session.amount_total / 100;
 
     await Booking.create({tour, user, price});
-};
+});
 
 exports.webhookCheckout = (req, res, next) => {
     const signature = req.headers['stripe-signature'];
