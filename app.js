30b6b9d0104c23df9ee8e8634e3c1963dba937fd
Abhishek vishwakarma <65114993+Abhishek-1337@users.noreply.github.com>
2023-08-18 14:18:00 +0530

    traditional code to remeber

--- app.js
+++ app.js
@@ -96,6 +96,15 @@ app.all('*', (req, res, next)=>{
     next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
+//app.use((err, req, res, next)=>{
+//     const statusCode = err.statusCode || 500;
+//     const status = err.status || 'error';
+
+//     res.status(statusCode).json({
+//         status: err.status,
+//         message: err.message
+//     })
+// }
 app.use(globalErrorHandler);
 
 module.exports = app;
